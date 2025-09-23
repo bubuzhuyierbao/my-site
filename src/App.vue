8dd70a4422abfb42d6027abd12df1708374f4b92
黄继波 <1043903537@qq.com>
2025-09-23 16:40:11 +0800

    v3ts

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- 路由出口：当前匹配到的页面组件会显示在这里 -->
+    <router-view />
+
+    <!-- 你也可以在这里加全局 footer 等 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// App.vue 本身一般不写逻辑，只写全局布局
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import Home from '@/views/Home/index.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
+
